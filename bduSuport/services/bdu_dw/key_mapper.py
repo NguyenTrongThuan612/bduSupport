18311f29b51a11a9968155498786b051a7cdb1d3
thuannt thuannt <thuannt@vnpay.vn>
2025-07-31 21:02:47 +0700

    fix bug

--- bduSuport/services/bdu_dw/key_mapper.py
+++ bduSuport/services/bdu_dw/key_mapper.py
@@ -1,5 +1,10 @@
 def convert_keys(data: dict, key_mapping: dict) -> dict:
-    return {key_mapping.get(key, key): value for key, value in data.items()}
+    converted_data = {}
+    for key, value in data.items():
+        if key in key_mapping:
+            converted_data[key_mapping[key]] = value
+
+    return converted_data
 
 def convert_list(data: list, key_mapping: dict) -> list:
     return [convert_keys(item, key_mapping) for item in data]
